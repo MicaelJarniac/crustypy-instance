12a0674227ea50f400595d6fce5fb258c82e41b4
Micael Jarniac <micael@jarniac.com>
2023-08-23 10:16:59 -0300

    feat: add benchmarking tools (#10)
    * feat: add benchmarking tools
    
    WIP
    
    Closes #9
    
    * feat: benchmarking
    
    * chore: remove asv from Nox
    
    Will consider whether it should be added again later, or if it should be run separately.
    
    * build(deps): separate asv from tests

---
+++ benchmarks/__init__.py
@@ -0,0 +1,1 @@
+"""Benchmarking for crustypy-instance."""
--- tests/test_crustypy_instance.py
+++ tests/test_crustypy_instance.py
@@ -1,11 +1,11 @@
 """Testing for crustypy-instance."""
 
+from pytest_benchmark.fixture import BenchmarkFixture  # type: ignore[import]
+
 from crustypy_instance import make_greeting
 
 
-def test_make_greeting() -> None:
+def test_make_greeting(benchmark: BenchmarkFixture) -> None:
     """Test `make_greeting`."""
-    assert (
-        make_greeting("Micael Jarniac")
-        == "Hello, Micael Jarniac. Welcome to your new project!"
-    )
+    result = benchmark(make_greeting, "Micael Jarniac")
+    assert result == "Hello, Micael Jarniac. Welcome to your new project!"
